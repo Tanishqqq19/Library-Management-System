1b2c2f625a465d4f1ad9ce3f77c079fb28005a2a
Tanishq Malhotra <61560725+Tanishqqq19@users.noreply.github.com>
2024-08-06 10:27:47 +0530

    update

--- routes/manage_returns.py
+++ routes/manage_returns.py
@@ -10,6 +10,7 @@ def manage_returns_():
 
     database_connection = None
     user_id = session["user_id"]
+    from_date=''
 
     try:
         with sql.connect("library.db") as database_connection:
@@ -23,16 +24,23 @@ def manage_returns_():
             for record in unreturned_books:
                 for book_id in record:
                     cursor.execute(
-                        "SELECT image, book_id, author FROM books WHERE book_id=?",
+                        "SELECT image, book_id, author, books_name FROM books WHERE book_id=?",
                         [book_id],
                     )
                     book_details = cursor.fetchall()
                     for detail in book_details:
                         book_details_list.append(detail)
+                    print(book_details_list)
+
+                    cursor.execute('Select to_date FROM records WHERE book_user_id=?',[book_id])
+                    from_date=cursor.fetchall()
+                    print(from_date)
+                    print(from_date[-1])
+
     except Exception as e:
         if database_connection:
             database_connection.rollback()
     finally:
         if database_connection:
             database_connection.close()
-        return render_template("return.html", book_details_list=book_details_list)
+        return render_template("return.html", book_details_list=book_details_list,from_date=from_date)
