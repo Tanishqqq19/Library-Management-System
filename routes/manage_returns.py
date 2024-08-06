d5a186a62f91717ef416564096daef09a6f47359
Tanishq Malhotra <61560725+Tanishqqq19@users.noreply.github.com>
2024-08-06 23:16:00 +0530

    return update done

--- routes/manage_returns.py
+++ routes/manage_returns.py
@@ -15,27 +15,36 @@ def manage_returns_():
     try:
         with sql.connect("library.db") as database_connection:
             cursor = database_connection.cursor()
+            print(111)
             cursor.execute(
-                'SELECT book_user_id FROM records WHERE book_returned="No" AND borrow_user_id=?',
+                'SELECT book_user_id, to_date FROM records WHERE book_returned="No" AND borrow_user_id=?',
                 [user_id],
             )
+            print(2)
             unreturned_books = cursor.fetchall()
             book_details_list = []
             for record in unreturned_books:
-                for book_id in record:
-                    cursor.execute(
-                        "SELECT image, book_id, author, books_name FROM books WHERE book_id=?",
-                        [book_id],
-                    )
-                    book_details = cursor.fetchall()
-                    for detail in book_details:
-                        book_details_list.append(detail)
-                    print(book_details_list)
+                print(record)
+                print('----')
+                # for book_id in record:
+                cursor.execute(
+                    "SELECT image, book_id, author, books_name FROM books WHERE book_id=?",
+                    [record[0]],
+                )
+                detail_add=[]
+                book_details = cursor.fetchall()
+                for detail in book_details:
+                    for i in detail:
+                        detail_add.append(i)
+                    detail_add.append(record[1])
+                    print(detail_add)
+                    book_details_list.append(detail_add)
+                    # print(book_details_list)
 
-                    cursor.execute('Select to_date FROM records WHERE book_user_id=?',[book_id])
-                    from_date=cursor.fetchall()
-                    print(from_date)
-                    print(from_date[-1])
+                    # cursor.execute('Select to_date FROM records WHERE book_user_id=?',[book_id])
+                    # from_date=cursor.fetchall()
+                    # print(from_date)
+                    # print(from_date[-1])
 
     except Exception as e:
         if database_connection:
